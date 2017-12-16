5e3e6d634160676c459c61b0ceeee9565332f8cb
Brian Cappello <briancappello@gmail.com>
2017-12-16 01:04:35 -0500

    update to flask-unchained

--- flask_celery_bundle/__init__.py
+++ flask_celery_bundle/__init__.py
@@ -1,9 +1,8 @@
-from flask_application_factory import Bundle
+from flask_unchained import Bundle
 
 from .extension import celery
 
 
 class FlaskCeleryBundle(Bundle):
-    module_name = __name__
     name = 'celery'
     extensions_module_name = 'extension'
