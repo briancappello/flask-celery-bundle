993ab352a03ffbcc42d07a2f629db5ec1b5411a4
Brian Cappello <briancappello@gmail.com>
2018-04-07 22:03:31 -0400

    release 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='Flask Celery Bundle',
-    version='0.1.0',
+    version='0.2.0',
     description='Adds Celery support to Flask Unchained',
     long_description=long_description,
     url='https://github.com/briancappello/flask-celery-bundle',
